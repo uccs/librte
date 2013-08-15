6d69363d810aa7781bed45bcc8f3c0aa05acdcd6
Swen Boehm <bohms@ornl.gov>
2013-08-15 15:13:26 -0400

    + create autoconf and autoconf/m4 directories if they do not exist

--- autogen.pl
+++ autogen.pl
@@ -1,5 +1,21 @@
 #!/usr/bin/env perl
 
+use Cwd;
+
+my $dir = getcwd;
+
+if (!-d "autoconf") {
+    print "Creating autoconf directory\n";
+    my $cmd = "mkdir -p autoconf/m4";
+    system($cmd);
+}
+
+if (!-d "autoconf/m4") {
+    print "Creating autoconf/m4 directory.\n";
+    my $cmd = "mkdir autoconf/m4";
+    system($cmd);
+}
+
 # Run autoreconf
 print "==> Running autoreconf\n";
 my $cmd = "autoreconf -ivf --warnings=all,no-obsolete,no-override -I autoconf";
