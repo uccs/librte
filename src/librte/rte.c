bff5b14c5e9e1832a692cedccebba2c09b4cb494
Swen Boehm <bohms@ornl.gov>
2013-08-20 13:16:23 -0400

    + initialize the function table to the opal back end

--- src/librte/rte.c
+++ src/librte/rte.c
@@ -13,6 +13,10 @@
 #include <rte.h>
 #include <stdarg.h>
 
+/* include the headers for the backends  */
+
+#include "src/backend/orte/rte_orte.h"
+
 /**
  * @file
  *
@@ -94,7 +98,7 @@ typedef int (*rte_be_srs_exchange_data_fn_t)(rte_srs_session_t session);
 
 typedef void (*rte_be_progress_fn_t)(void);
 
-/* This is the lookup table into the backend implementation */
+/* This is the lookup table into the back end implementation */
 struct rte_back_end_fn_table_s
 {
     rte_be_init_fn_t                rte_be_init;
@@ -129,6 +133,37 @@ struct rte_back_end_fn_table_s rte_back_end_fn_table;
 
 int rte_init(int *argc, char ***argv, rte_group_t *out_group)
 {
+    /* we need to figure out which back end started us and initialize the
+       table accordingly */
+
+    /* for now we hard code orte */
+    rte_back_end_fn_table.rte_be_init                = rte_orte_init;
+    rte_back_end_fn_table.rte_be_finalize            = rte_orte_finalize;
+    rte_back_end_fn_table.rte_be_abort               = rte_orte_abort;
+    rte_back_end_fn_table.rte_be_get_my_ec           = rte_orte_get_my_ec;
+    rte_back_end_fn_table.rte_be_group_index_to_ec   = rte_orte_group_index_to_ec;
+    rte_back_end_fn_table.rte_be_group_size          = rte_orte_group_size;
+    rte_back_end_fn_table.rte_be_group_rank          = rte_orte_group_rank;
+    rte_back_end_fn_table.rte_be_get_ec_locality     = rte_orte_get_ec_locality;
+    rte_back_end_fn_table.rte_be_cmp_ec              = rte_orte_cmp_ec;
+    rte_back_end_fn_table.rte_be_get_ec_index        = rte_orte_get_ec_index;
+    rte_back_end_fn_table.rte_be_get_ec_node_name    = rte_orte_get_ec_node_name;
+    rte_back_end_fn_table.rte_be_get_ec_hostname     = rte_orte_get_ec_hostname;
+    rte_back_end_fn_table.rte_be_get_session_dir     = rte_orte_get_session_dir;
+    rte_back_end_fn_table.rte_be_cancel              = rte_orte_cancel;
+    rte_back_end_fn_table.rte_be_unpack              = rte_orte_unpack;
+    rte_back_end_fn_table.rte_be_recv                = rte_orte_recv;
+    rte_back_end_fn_table.rte_be_recv_nbcb           = rte_orte_recv_nbcb;
+    rte_back_end_fn_table.rte_be_send                = rte_orte_send;
+    rte_back_end_fn_table.rte_be_send_nbcb           = rte_orte_send_nbcb;
+    rte_back_end_fn_table.rte_be_barrier             = rte_orte_barrier;
+    rte_back_end_fn_table.rte_be_srs_session_create  = rte_orte_srs_session_create;
+    rte_back_end_fn_table.rte_be_srs_session_destroy = rte_orte_srs_session_destroy;
+    rte_back_end_fn_table.rte_be_srs_get_data        = rte_orte_srs_get_data;
+    rte_back_end_fn_table.rte_be_srs_set_data        = rte_orte_srs_set_data;
+    rte_back_end_fn_table.rte_be_srs_exchange_data   = rte_orte_srs_exchange_data;
+    rte_back_end_fn_table.rte_be_progress            = rte_orte_progress;
+
     return rte_back_end_fn_table.rte_be_init(argc, argv, out_group);
 }
 
