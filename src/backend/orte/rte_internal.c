a9123a437a03475e92ad1e2225efff86573f1e12
Swen Boehm <bohms@ornl.gov>
2013-09-10 13:51:25 -0400

    Remove query to opal_db from proc_init.
    The modex exchange is done outside of rte_init. So we can not access any modex data
    after the exchange is done.

--- src/backend/orte/rte_internal.c
+++ src/backend/orte/rte_internal.c
@@ -14,6 +14,7 @@ opal_pointer_array_t rte_procs;
 static void rte_orte_proc_construct(rte_proc_t* proc)
 {
     proc->hostname = NULL;
+    proc->locality = RTE_PROC_LOCALITY_UNKNOWN;
 }
 
 static void rte_orte_proc_destruct(rte_proc_t* proc)
@@ -59,58 +60,6 @@ int all_orte_proc_init(void)
         if (i == orte_process_info.my_name.vpid) {
             proc->hostname = strdup(orte_process_info.nodename);
         }
-
-        if (ORTE_SUCCESS != (rc = opal_db.store((opal_identifier_t*)&proc->name,
-            OPAL_DB_INTERNAL, ORTE_DB_DAEMON_VPID, &daemon, OPAL_UINT32)))
-        {
-            OBJ_DESTRUCT(&rte_procs);
-            return RTE_ERROR;
-        }
-        else {
-            rc = opal_db.fetch_pointer((opal_identifier_t*)&(proc->name), 
-                    ORTE_DB_HOSTNAME, 
-                    (void**)&(proc->hostname), 
-                    OPAL_STRING);
-            if (ORTE_SUCCESS != rc)
-                return RTE_ERROR;
-        }
-
-#if  OPAL_HAVE_HWLOC
-        /* retrieve the binding for the other proc */
-        rc = opal_db.fetch((opal_identifier_t*)&(proc->name), ORTE_DB_CPUSET,
-                           (void**)&cpu_bitmap, OPAL_STRING);
-        if (ORTE_SUCCESS != rc) {
-            free(cpu_bitmap);
-            return RTE_ERROR;
-        }
-
-        if (OPAL_EQUAL == orte_util_compare_name_fields(ORTE_NS_CMP_ALL,
-            &proc->name, ORTE_PROC_MY_NAME)) {
-            /* if this data is from myself, then set
-             * locality to all */
-            proc->locality = OPAL_PROC_ALL_LOCAL;
-        } else if (daemon != ORTE_PROC_MY_DAEMON->vpid) {
-            proc->locality = OPAL_PROC_ALL_LOCAL;
-        } else if (NULL == cpu_bitmap || NULL == orte_process_info.cpuset) {
-            proc->locality = OPAL_PROC_ON_NODE;
-        } else {
-            /* we share a node - see what else we share */
-            proc->locality = opal_hwloc_base_get_relative_locality(
-                                                       opal_hwloc_topology,
-                                                       orte_process_info.cpuset,
-                                                       cpu_bitmap);
-        }
-#else
-        if (OPAL_EQUAL == orte_util_compare_name_fields(ORTE_NS_CMP_ALL,
-            &proc->name, ORTE_PROC_MY_NAME)) {
-            proc->locality = OPAL_PROC_ALL_LOCAL;
-        } else if (daemon != ORTE_PROC_MY_DAEMON->vpid) {
-            proc->locality = OPAL_PROC_NON_LOCAL;
-        } else {
-            proc->locality = OPAL_PROC_ON_NODE;
-        }
-#endif
-
     }
     return RTE_SUCCESS;
 }
