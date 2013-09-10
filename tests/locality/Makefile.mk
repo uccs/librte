f8205735d836e2638e6d0cd12f9f4f3330469a72
Swen Boehm <bohms@ornl.gov>
2013-09-10 10:59:23 -0400

    Add a test to print out a processes locality.

--- tests/locality/Makefile.mk
+++ tests/locality/Makefile.mk
@@ -1,4 +1,10 @@
-bin_PROGRAMS += rte_locality_test
+## Copyright (c) 2013      UT-Battelle, LLC. All rights reserved.
+##                         All rights reserved.
+
+bin_PROGRAMS += rte_locality_test rte_locality_to_str_test
 
 rte_locality_test_SOURCES = tests/locality/rte_locality.c
 rte_locality_test_LDADD = librte.la
+
+rte_locality_to_str_test_SOURCES = tests/locality/rte_locality_to_str.c
+rte_locality_to_str_test_LDADD = librte.la
