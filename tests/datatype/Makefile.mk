66f4eefa57eb10ca41a4d2874fee9c43d7664987
Swen Boehm <bohms@ornl.gov>
2013-09-10 11:01:04 -0400

    Add copyright notice.

--- tests/datatype/Makefile.mk
+++ tests/datatype/Makefile.mk
@@ -1,3 +1,5 @@
+## Copyright (c) 2013      UT-Battelle, LLC. All rights reserved.
+
 bin_PROGRAMS += rte_get_datatype_size_test
 
 rte_get_datatype_size_test_SOURCES = tests/datatype/rte_get_datatype_size.c
