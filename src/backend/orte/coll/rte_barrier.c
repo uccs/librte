0c7ffe752542fa35b54b147fd03ad8ae6b27a9d8
Swen Boehm <bohms@ornl.gov>
2013-09-11 14:40:07 -0400

    Fix problem with barrier related to changes in ompi trunk.

--- src/backend/orte/coll/rte_barrier.c
+++ src/backend/orte/coll/rte_barrier.c
@@ -1,10 +1,10 @@
 /*
- * Copyright (c)           2011 UT-Battelle, LLC. All rights reserved.
+ * Copyright (c) 2011-2013 UT-Battelle, LLC. All rights reserved.
  *                         All rights reserved.
  * $COPYRIGHT$
- * 
+ *
  * Additional copyrights may follow
- * 
+ *
  * $HEADER$
  *
  */
@@ -17,17 +17,21 @@
 int rte_orte_barrier (rte_group_t group)
 {
     int rc;
-    orte_grpcomm_collective_t coll;
+    orte_grpcomm_collective_t *coll;
 
-    OBJ_CONSTRUCT(&coll, orte_grpcomm_collective_t);
-    coll.id = orte_process_info.peer_init_barrier;
-    opal_progress_event_users_increment();
-    if (OPAL_SUCCESS != (rc = orte_grpcomm.barrier(&coll))) {
+    coll = OBJ_NEW(orte_grpcomm_collective_t);
+    coll->id = orte_grpcomm_base_get_coll_id ();// orte_process_info.peer_init_barrier;
+
+    if (OPAL_SUCCESS != (rc = orte_grpcomm.barrier(coll))) {
         return RTE_ERROR;
-    }   
-    while (coll.active) {
-        rte_progress();
-    }   
-    opal_progress_event_users_decrement();
+    }
+
+    coll->active = true;
+    while (coll->active) {
+        opal_progress();
+    }
+
+    OBJ_RELEASE(coll);
+
     return RTE_SUCCESS;
 }
