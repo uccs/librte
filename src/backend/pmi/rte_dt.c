4014de839d8abd8dd8f61575543eed4627ebeb7e
Swen Boehm <bohms@ornl.gov>
2013-12-12 12:30:40 -0500

    fix wrong datatype sizes

--- src/backend/pmi/rte_dt.c
+++ src/backend/pmi/rte_dt.c
@@ -15,16 +15,16 @@
 #include <string.h>
 
 
-const struct rte_dt_t rte_int1       = {rte_pmi_int8, 8};
-const struct rte_dt_t rte_int2       = {rte_pmi_int16, 16};
-const struct rte_dt_t rte_int4       = {rte_pmi_int32, 32};
-const struct rte_dt_t rte_int8       = {rte_pmi_int64, 64};
-const struct rte_dt_t rte_uint1      = {rte_pmi_uint8, 8};
-const struct rte_dt_t rte_uint2      = {rte_pmi_uint16, 16};
-const struct rte_dt_t rte_uint4      = {rte_pmi_uint32, 32};
-const struct rte_dt_t rte_uint8      = {rte_pmi_uint64, 64};
-const struct rte_dt_t rte_float2     = {rte_pmi_float2, 64};
-const struct rte_dt_t rte_bool       = {rte_pmi_bool, 8};
+const struct rte_dt_t rte_int1       = {rte_pmi_int8, 1};
+const struct rte_dt_t rte_int2       = {rte_pmi_int16, 2};
+const struct rte_dt_t rte_int4       = {rte_pmi_int32, 4};
+const struct rte_dt_t rte_int8       = {rte_pmi_int64, 8};
+const struct rte_dt_t rte_uint1      = {rte_pmi_uint8, 1};
+const struct rte_dt_t rte_uint2      = {rte_pmi_uint16, 2};
+const struct rte_dt_t rte_uint4      = {rte_pmi_uint32, 2};
+const struct rte_dt_t rte_uint8      = {rte_pmi_uint64, 8};
+const struct rte_dt_t rte_float2     = {rte_pmi_float2, 8};
+const struct rte_dt_t rte_bool       = {rte_pmi_bool, 1};
 
 int rte_pmi_unpack (rte_iovec_t     *data,
                     void            *src,
