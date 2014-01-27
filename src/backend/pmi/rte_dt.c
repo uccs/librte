958e56a70e2e35ab3cc4b260f9d5944ffc3a2865
Swen Boehm <bohms@ornl.gov>
2014-01-27 16:15:56 -0500

    Fix bug in addressing offsets in the buffer

--- src/backend/pmi/rte_dt.c
+++ src/backend/pmi/rte_dt.c
@@ -64,36 +64,43 @@ int rte_pmi_unpack (rte_iovec_t     *data,
                     prep_buffer (base_ptr, &tmp);
                     nb = sscanf (base_ptr, "%u", (uint8_t*)(data_ptr));
                     base_ptr = tmp;
+                    data_ptr++;
                     break;
                 case rte_pmi_int16:
                     prep_buffer (base_ptr, &tmp);
                     nb = sscanf (base_ptr, "%d", (int16_t*)(data_ptr));
                     base_ptr = tmp;
+                    data_ptr++;
                     break;
                 case rte_pmi_uint16:
                     prep_buffer (base_ptr, &tmp);
                     nb = sscanf (base_ptr, "%u", (uint16_t*)(data_ptr));
                     base_ptr = tmp;
+                    data_ptr++;
                     break;
                 case rte_pmi_int32:
                     prep_buffer (base_ptr, &tmp);
                     nb = sscanf (base_ptr, "%d", (int32_t*)(data_ptr));
                     base_ptr = tmp;
+                    data_ptr++;
                     break;
                 case rte_pmi_uint32:
                     prep_buffer (base_ptr, &tmp);
                     nb = sscanf (base_ptr, "%u", (uint32_t*)(data_ptr));
                     base_ptr = tmp;
+                    data_ptr++;
                     break;
                 case rte_pmi_int64:
                     prep_buffer (base_ptr, &tmp);
                     nb = sscanf (base_ptr, "%ld", (int64_t*)(data_ptr));
                     base_ptr = tmp;
+                    data_ptr++;
                     break;
                 case rte_pmi_uint64:
                     prep_buffer (base_ptr, &tmp);
                     nb = sscanf (base_ptr, "%lu", (uint64_t*)(data_ptr));
                     base_ptr = tmp;
+                    data_ptr++;
                     break;
                 case rte_pmi_float2:
                     break;
